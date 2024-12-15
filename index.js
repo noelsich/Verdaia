8ff2461d1b3b8da0de7f832b8a4dd9e0cbd474e8
noelsich <123343039+noelsich@users.noreply.github.com>
2024-12-15 08:31:12 +0200

    Firsts Coommit

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+console.log("index.js is loaded");
+
+// Example fetch code
+fetch('./header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading header:', error));
+
+fetch('./footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading footer:', error));
